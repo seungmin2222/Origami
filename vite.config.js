b9615c6d3f67f80f3ba38b58e9b4aa5181a1950e
Joohyeji <rosyjoo1999@gmail.com>
2024-06-17 21:59:57 +0900

    🔧 Rename: 폴더 구조 변경 - index.html 루트 위치로 변경

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
+/* eslint-disable */
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: 'public',
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true,
-  },
   server: {
     port: 3000,
-    open: true,
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
   },
 });
