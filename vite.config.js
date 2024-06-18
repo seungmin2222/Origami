7ce09876d99eeaad6eb77346d99aa9242ae2cf1a
seungminlee <seungmin@seungminleeui-noteubug.local>
2024-06-18 14:25:12 +0900

    📝 Docs: #6 완료 페이지 vite.config 수정

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
+        completePage: resolve(__dirname, 'completePage.html'),
       },
     },
   },
