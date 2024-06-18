98c740826cd440f27a0ff5a394f55c37ba1e121c
Joohyeji <rosyjoo1999@gmail.com>
2024-06-18 22:10:27 +0900

    ✨ Feat: #3 메인 페이지 기능 추가 - 사운드버튼 토글, 모드 판별 기능

--- src/components/main.js
+++ src/components/main.js
@@ -1,9 +1,16 @@
 const $soundButton = document.querySelector('.sound-button');
 const $mainBgm = document.querySelector('.main-bgm');
-let isMute = false;
 
-const changeSoundButton = isMute => {
-  if (isMute) {
+const $sandboxModeButton = document.getElementById('sandboxMode');
+const $guideModeButton = document.getElementById('guideMode');
+const $modeModal = document.querySelector('.mode-blur-bg');
+
+const $guideModeBox = document.querySelector('.guide-mode-box');
+
+let isMuted = false;
+
+const toggleSoundButton = isMuted => {
+  if (isMuted) {
     $soundButton.style.backgroundPosition = '-66px -108px';
     $mainBgm.pause();
   } else {
@@ -13,6 +20,48 @@ const changeSoundButton = isMute => {
 };
 
 $soundButton.addEventListener('click', () => {
-  isMute = !isMute;
-  changeSoundButton(isMute);
+  isMuted = !isMuted;
+  toggleSoundButton(isMuted);
+});
+
+const handleModeButtonClick = (clickedButton, otherButton) => {
+  clickedButton.classList.add('active');
+  otherButton.classList.remove('active');
+};
+
+const removeCheckIcon = buttons => {
+  buttons.forEach(button => {
+    button.classList.remove('active');
+    const checkIcon = button.querySelector('.check-icon');
+    if (checkIcon) {
+      button.removeChild(checkIcon);
+    }
+  });
+};
+
+const addCheckIcon = button => {
+  button.classList.add('active');
+
+  const checkIcon = document.createElement('div');
+  checkIcon.classList.add('check-icon');
+
+  button.appendChild(checkIcon);
+};
+
+$sandboxModeButton.addEventListener('click', () => {
+  handleModeButtonClick($sandboxModeButton, $guideModeButton);
+  $modeModal.style.opacity = '0';
+});
+
+$guideModeButton.addEventListener('click', () => {
+  handleModeButtonClick($guideModeButton, $sandboxModeButton);
+  $modeModal.style.opacity = '1';
+});
+
+$guideModeBox.addEventListener('click', event => {
+  const $guideModeButtons = document.querySelectorAll('.guide-mode');
+  const $guideModeButton = event.target.closest('.guide-mode');
+
+  removeCheckIcon($guideModeButtons);
+  addCheckIcon($guideModeButton);
 });
