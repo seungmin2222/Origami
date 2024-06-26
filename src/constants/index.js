8fbb938bbdda711e43cc9418731e5b2beb8e45d5
Jeongsohee <toy3045@gmail.com>
2024-06-26 19:56:32 +0900

    💄 Design: 컬러값 소문자로 수정

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
-const POINTS_MARKER_COLOR = '#098CEA';
-const RED_MARKER_COLOR = '#FF0000';
+const POINTS_MARKER_COLOR = '#098cea';
+const RED_MARKER_COLOR = '#ff0000';
 const PAPERCOLORS = [
   0x1aa24d, 0xf2482e, 0x343394, 0xfed53d, 0x000000, 0xff9fd5, 0x9ddef4,
   0xff8158,
