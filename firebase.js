ef1e20bcaeefa43e8a990a4b3c710ae3d34ca112
Joohyeji <rosyjoo1999@gmail.com>
2024-06-29 16:16:06 +0900

    ✨ Feat: #18 firebase 환경 변수 추가

--- firebase.js
+++ firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyD3oZG97hnma9qtfFW4DPTe-kmJJgnsraQ',
-  authDomain: 'origami-b2b79.firebaseapp.com',
-  projectId: 'origami-b2b79',
-  storageBucket: 'origami-b2b79.appspot.com',
-  messagingSenderId: '1010489901720',
-  appId: '1:1010489901720:web:6fbe2122e63a5f054786ed',
-  measurementId: 'G-PVFN523QV5',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
