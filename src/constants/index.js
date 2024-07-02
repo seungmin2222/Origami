4639786912b276271f3fa16b27674c54ed9510a0
Jeongsohee <toy3045@gmail.com>
2024-07-03 01:40:39 +0900

    ♻️ Refactor: #66 툴팁 텍스트 변경 및 추가

--- src/constants/index.js
+++ src/constants/index.js
@@ -42,10 +42,12 @@ const TOOLTIP_MESSAGES = {
   RESTART_TOOLTIP: '현재 모드의 처음 단계로 이동합니다',
   FINISH_TOOLTIP: '종이접기를 완료합니다',
   UNFOLD_TOOLTIP: '종이를 펼칩니다',
-  PRESTEP_TOOLTIP: '전 단계로 돌아갑니다',
+  PRESTEP_TOOLTIP: '이전 단계로 돌아갑니다',
   NEXTSTEP_TOOLTIP: '다음 단계로 돌아갑니다',
   SHARE_TOOLTIP: '공유할 수 있습니다',
   MODESELECT_TOOLTIP: '클릭하면 해당 모드로 이동합니다',
+  NEXT_TOOLTIP: '다음 스텝 가이드를 보여줍니다',
+  PREV_TOOLTIP: '이전 스텝 가이드를 보여줍니다',
 };
 
 export {
