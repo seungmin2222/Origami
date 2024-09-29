c0a605efdc539a54265641e8ab2cc3ec708b31ed
Jeongsohee <toy3045@gmail.com>
2024-09-29 19:00:57 +0900

    ✨ Feat: getUserService 함수들 구현
    - fetchUserDocument 함수 생성하여 사용자 정보 가져오기
    - fetchAllUserDocuments 함수 생성하여 모든 완료 종이접기 데이터 가져오기
    - fetchUserPositions 함수 생성하여 청크화된 사용자의 완료 종이접기 3D 좌표 가져오기

---
+++ src/utils/getUserService.js
@@ -0,0 +1,53 @@
+import { collection, doc, getDoc, getDocs } from 'firebase/firestore';
+import { db } from './firebaseConfig';
+
+const fetchUserDocument = async id => {
+  const docRef = doc(db, 'users', id);
+  const docSnap = await getDoc(docRef);
+
+  if (docSnap.exists()) {
+    return docSnap.data();
+  } else {
+    console.error('데이터가 없습니다.');
+    return null;
+  }
+};
+
+const fetchAllUserDocuments = async () => {
+  try {
+    const querySnapshot = await getDocs(collection(db, 'users'));
+    const allDocuments = [];
+
+    querySnapshot.docs.forEach(doc => {
+      const data = doc.data();
+      allDocuments.push({ id: doc.id, ...data });
+    });
+
+    return allDocuments;
+  } catch (error) {
+    console.error(
+      '사용자의 데이터를 처리하는 도중 에러가 발생했습니다.',
+      error
+    );
+    throw error;
+  }
+};
+
+const fetchUserPositions = async userId => {
+  try {
+    const usersCollectionRef = collection(db, 'users', userId, 'positions');
+    const usersCoordinatedRef = await getDocs(usersCollectionRef);
+    const positionsPromises = usersCoordinatedRef.docs.map(doc => {
+      return doc.data().positions;
+    });
+
+    const positionsData = await Promise.all(positionsPromises);
+
+    return positionsData;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export { fetchAllUserDocuments, fetchUserDocument, fetchUserPositions };
