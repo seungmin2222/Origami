040a8d213eef0ed6f478e796e8398617958c8264
Joohyeji <rosyjoo1999@gmail.com>
2024-10-03 19:49:59 +0900

    ♻️ Refactor: 사용하지않는 코드 삭제

--- src/components/three/utils/paperUtils.js
+++ src/components/three/utils/paperUtils.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 import { findClickClosestVertex } from '../BorderPoints';
 
-export const calculateMousePosition = (event, camera) => {
+export const calculateMousePosition = event => {
   const mouse = new THREE.Vector2();
   mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
   mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
